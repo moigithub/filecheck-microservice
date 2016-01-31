4724220edf19d60ac73b15a0ccb4cbc39d21761d
Moises Man <mcarlosman@live.com>
2016-01-31 09:46:45 -0500

    initial commit

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,57 +1,24 @@
 'use strict';
+var multer = require('multer');
+var fs= require('fs');
+var upload = multer({ dest: 'public/' });
 
-var path = process.cwd();
-var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
+module.exports = function (app) {
 
-module.exports = function (app, passport) {
+	app.post('/api/fileanalyse', upload.single('thefile'), function (req, res, next) {
+	  // req.file is the `avatar` file 
+	  // req.body will hold the text fields, if there were any 
+	  console.log("body>>",req.body);
+	  console.log("files>>",req.file);
 
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
-		}
-	}
+	  if(req.file){
+	  	var fileSize = req.file.size;
+	  	// delete file
+	  	fs.unlink(req.file.filename, function(){});
 
-	var clickHandler = new ClickHandler();
+	  	return res.status(204).end(fileSize);
+	  }
+	  return res.status(204).end('Error: Invalid file.');
+	});
 
-	app.route('/')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/index.html');
-		});
-
-	app.route('/login')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
-		});
-
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
-		});
-
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
-
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
-
-	app.route('/auth/github')
-		.get(passport.authenticate('github'));
-
-	app.route('/auth/github/callback')
-		.get(passport.authenticate('github', {
-			successRedirect: '/',
-			failureRedirect: '/login'
-		}));
-
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
 };
