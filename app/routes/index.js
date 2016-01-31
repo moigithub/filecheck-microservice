952b098acef84b1828eb64850fe619348e82ee27
Moises Man <mcarlosman@live.com>
2016-01-31 10:18:40 -0500

    small fixes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,24 +1,26 @@
 'use strict';
 var multer = require('multer');
 var fs= require('fs');
-var upload = multer({ dest: 'public/' });
+var upload = multer({ dest: 'temp/' });
 
 module.exports = function (app) {
 
 	app.post('/api/fileanalyse', upload.single('thefile'), function (req, res, next) {
 	  // req.file is the `avatar` file 
 	  // req.body will hold the text fields, if there were any 
-	  console.log("body>>",req.body);
-	  console.log("files>>",req.file);
+
+	  //console.log("body>>",req.body);
+	  //console.log("files>>",req.file);
 
 	  if(req.file){
 	  	var fileSize = req.file.size;
 	  	// delete file
+	  	console.log('deleting.. '+req.file.filename);
 	  	fs.unlink(req.file.filename, function(){});
 
-	  	return res.status(204).end(fileSize);
+	  	return res.send(fileSize.toString());
 	  }
-	  return res.status(204).end('Error: Invalid file.');
+	  return res.status(400).send('Invalid file.');
 	});
 
 };
